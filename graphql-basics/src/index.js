3860fa81cab022010035d300bf6d2990618ea7b4
Raymond Flores <raymondfflores@gmail.com>
2020-06-24 00:53:21 -0500

    relational data

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,9 +2,68 @@ import { GraphQLServer } from 'graphql-yoga'
 
 // Scalar types - String, Boolean, Int, Float, ID
 
+// Demo user data
+const users = [
+  {
+    id: '1',
+    name: 'Raymond',
+    email: 'ray@aol.com',
+    age: 27
+  },
+  {
+    id: '2',
+    name: 'Devin',
+    email: 'dev@aol.com',
+    age: 7
+  }
+]
+
+// Demo post data
+const posts = [
+  {
+    id: '1',
+    title: 'My first post',
+    body: 'hello world',
+    published: true,
+    author: '1'
+  },
+  {
+    id: '2',
+    title: 'My 2nd post',
+    body: 'this is the body',
+    published: false,
+    author: '2'
+  }
+]
+
+// Demo comment data
+const comments = [
+  {
+    id: '1',
+    text: 'first comment',
+    author: '1',
+    post: '1'
+  },
+  {
+    id: '2',
+    text: 'second comment',
+    author: '1',
+    post: '1'
+  },
+  {
+    id: '3',
+    text: 'hello world',
+    author: '2',
+    post: '2'
+  }
+]
+
 // Type definitions (schema)
 const typeDefs = `
   type Query {
+    users(query: String): [User!]!
+    posts(query: String): [Post!]!
+    comments: [Comment!]!
     me: User!
     post: Post!
   }
@@ -14,6 +73,8 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
+    comments: [Comment!]!
   }
 
   type Post {
@@ -21,12 +82,49 @@ const typeDefs = `
     title: String!
     body: String!
     published: Boolean!
+    author: User!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    author: User!
+    post: Post!
   }
 `
 
 // Resolvers
 const resolvers = {
   Query: {
+    users(parent, { query }, ctx, info) {
+      if (!query) {
+        return users
+      }
+
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(query.toLowerCase())
+      )
+    },
+    posts(parent, { query }, ctx, info) {
+      if (!query) {
+        return posts
+      }
+
+      return posts.filter((post) => {
+        const isTitleMatch = post.title
+          .toLowerCase()
+          .includes(query.toLowerCase())
+        const isBodyMatch = post.body
+          .toLowerCase()
+          .includes(query.toLowerCase())
+
+        return isTitleMatch || isBodyMatch
+      })
+    },
+    comments(parent, args, ctx, info) {
+      return comments
+    },
     me() {
       return {
         id: '123',
@@ -42,6 +140,30 @@ const resolvers = {
         body: 'This is my first post',
         published: true
       }
+    }
+  },
+  Post: {
+    author({ author }, args, ctx, info) {
+      return users.find((user) => user.id === author)
+    },
+    comments({ id }, args, ctx, info) {
+      return comments.filter((comment) => comment.post === id)
+    }
+  },
+  User: {
+    posts({ id }, args, ctx, info) {
+      return posts.filter((post) => post.author === id)
+    },
+    comments({ id }, args, ctx, info) {
+      return comments.filter((comment) => comment.author === id)
+    }
+  },
+  Comment: {
+    author({ author }, args, ctx, info) {
+      return users.find((user) => user.id === author)
+    },
+    post({ post }, args, ctx, info) {
+      return posts.find((p) => p.id === post)
     }
   }
 }
